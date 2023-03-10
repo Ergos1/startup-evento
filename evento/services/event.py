b1c745066cde4bd6e131fcfb50443fd47617c053
ergossteam <ergossteam@gmail.com>
2023-03-11 04:41:24 +0600

    feat: add event not found validation

--- evento/services/event.py
+++ evento/services/event.py
@@ -1,7 +1,14 @@
+from fastapi import HTTPException
 
-from evento.types import CreateEventSchema, BasePaginationFilter, GetEventOutSchema
 from evento.database import session_scope
-from evento.models import Event, EventSchedule, EventCategory, events_to_events_categories_table
+from evento.models import (
+    Event,
+    EventCategory,
+    EventSchedule,
+    events_to_events_categories_table,
+)
+from evento.types import BasePaginationFilter, CreateEventSchema, GetEventOutSchema
+
 
 def create_event(payload: CreateEventSchema, user_id: int):
     with session_scope() as db:
@@ -9,42 +16,42 @@ def create_event(payload: CreateEventSchema, user_id: int):
             **payload.dict(exclude={"schedules": True, "category_ids": True}),
             creator_id=user_id,
         )
-        
+
+        # TODO: make it more reliable
         db.add(new_event)
         db.commit()
         db.refresh(new_event)
 
         for category in payload.categories:
             event_category = events_to_events_categories_table.insert().values(
-                event_id=new_event.id, 
+                event_id=new_event.id,
                 event_category_id=category.id,
             )
             db.execute(event_category)
 
         for schedule in payload.schedules:
             new_schedule = EventSchedule(
-                start_date= schedule.start_date,
+                start_date=schedule.start_date,
                 end_date=schedule.end_date,
                 event_id=new_event.id,
-                description=schedule.description
+                description=schedule.description,
             )
             db.add(new_schedule)
 
-        return 
-    
+        return
+
 
 def get_event_list(payload: BasePaginationFilter) -> list[GetEventOutSchema]:
     with session_scope() as db:
-        dto = []
-
         events = db.query(Event).offset(payload.offset()).limit(payload.size).all()
-        for i in range(len(events)):
-            dto.append(GetEventOutSchema.from_orm(events[i]))
-        
-        return dto
-    
+        return list(map(lambda event: GetEventOutSchema.from_orm(event), events))
+
 
 def get_event(event_id: int) -> GetEventOutSchema:
     with session_scope() as db:
-        event = db.query(Event).filter(Event.id == event_id).first()
+        event = db.query(Event).filter(Event.id == event_id).one_or_none()
+
+        if event is None:
+            raise HTTPException(400, "Event not found")
+
         return GetEventOutSchema.from_orm(event)
